869cb2b28cec1521986b77c12ebe9a6a09a8d5a1
Rogerio Prazeres <rogerio.prazeres@outlook.com.br>
2023-06-22 16:03:52 -0300

    version 2

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ provider "helm" {
 
 
 module "lab" {
-  source = "https://github.com/rogerio-tecinfo/terraform-module-aks.git?ref=v2"
+  source = " https://github.com/rogerio-tecinfo/terraform-module-aks.git"
   env                     = "lab"
   project_name            = "zeus"
   node_pool_name          = "default"
